a8fa6391c248d036d3af95dbf13163ada6f88d7d
Kashav Madan <kshvmdn@gmail.com>
2016-02-12 16:56:20 -0500

    Validate movie file with extensions, add basic error handling

--- movieman/movieman.py
+++ movieman/movieman.py
@@ -5,7 +5,15 @@ import sys
 from pprint import pprint
 from tabulate import tabulate
 
-OMDB_URL = 'http://www.omdbapi.com/?type=movie&plot=short&tomatoes=true&t={}&y={}'
+OMDB_URL = 'http://www.omdbapi.com/?type=movie&tomatoes=true&t={}&y={}'
+
+try:
+    MOVIEDIR = sys.argv[1]
+except IndexError as err:
+    print('Expected movie path')
+    sys.exit(1)
+
+VALID_EXT = ('.avi', '.m4v', '.mkv', '.mov', '.mp4', '.mpeg', '.wmv')
 
 
 def request_data(title, year):
@@ -15,10 +23,13 @@ def request_data(title, year):
 def get_titles(dir_):
     movies = list()
     for root, dirs, files in os.walk(dir_):
-        for file_ in files:
+        for file_ in [f for f in files if f.endswith(VALID_EXT)]:
+            title, year = os.path.splitext(file_)[0].rsplit(' ', 1)
             movies.append({
-                'title': os.path.splitext(file_)[0].rsplit(' ', 1),
-                'fpath': os.path.join(root, file_)
+                'title': title,
+                'year': year,
+                'fpath': os.path.join(root, file_),
+                'fname': file_
             })
     return movies
 
