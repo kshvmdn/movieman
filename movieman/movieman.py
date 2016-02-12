94bc6665fd3861c5fc506ccedff49308e0363539
Kashav Madan <kshvmdn@gmail.com>
2016-02-12 14:33:49 -0500

    Add os.walk for parsing movie titles/years; add json decoding for request content

--- movieman/movieman.py
+++ movieman/movieman.py
@@ -4,17 +4,26 @@ import os
 
 from pprint import pprint
 
-OMDB_URL = 'http://www.omdbapi.com/?t={}'
+OMDB_URL = 'http://www.omdbapi.com/?type=movie&plot=long&tomatoes=true&t={}&y={}'
 
 
-def get_movie_names(dir_):
-    import os
-    dir_ = '/Users/kashavmadan/Downloads/movies'  # temporary
-
-    for subdir, dirs, files in os.walk(dir_):
-        for file in files:
-            print(os.path.join(subdir, file))
+def main(dir_='/Users/kashavmadan/Downloads/movies'):
+    for movie in get_movie_titles(dir_):
+        data = get_imdb_data(movie['title'][0], movie['title'][1])
 
 
-def get_imdb_data(movie):
-    return requests.get(OMDB_URL.format(movie)).json
+def get_movie_titles(dir_):
+    movies = []
+    for root, dirs, files in os.walk(dir_):
+        for file_ in files:
+            movies.append({
+                'title': os.path.splitext(file_)[0].rsplit(' ', 1),
+                'fpath': os.path.join(root, file_)
+            })
+    return movies
+
+
+def get_imdb_data(title, year):
+    return requests.get(OMDB_URL.format(title, year)).json()
+
+main()
