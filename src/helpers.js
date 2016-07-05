a49eb1f82d234c1675c33b54c90a5ab83b7f2f84
Kashav Madan <kshvmdn@gmail.com>
2016-07-05 09:18:50 -0400

    Remove unnecessary import

--- src/helpers.js
+++ src/helpers.js
@@ -1,10 +1,8 @@
 import got from 'got';
-import Promise from 'pinkie-promise';
 
 export function request(opts) {
-  const base = 'http://www.omdbapi.com';
-  return got(base, {
-    json: true,
-    query: opts
+  return got('http://www.omdbapi.com', { 
+    json: true, 
+    query: opts,
   });
 }
